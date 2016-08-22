0b6a00abe3ab74ef8ffb91a07cd4b5dd9c6f69d2
Mike Duin <michael.s.duin@gmail.com>
2016-08-21 21:46:40 -0700

    change tutorial photo, reconfig current week filters

--- public/javascripts/controllers/picks.controller.js
+++ public/javascripts/controllers/picks.controller.js
@@ -37,6 +37,48 @@ function PickController (oddsService, picksService, resultsService, authService)
   vm.activePayCalc = activePayCalc;
   vm.mlFormat = mlFormat;
 
+  vm.weekSetter = function(MatchTime) {
+    if (moment(MatchTime).isBetween('2016-06-23', '2016-09-07')) {
+      return "Preseason"
+    } else if (moment(MatchTime).isBetween('2016-09-07', '2016-09-14')) {
+      return "Week 1"
+    } else if (moment(MatchTime).isBetween('2016-09-14', '2016-09-21')) {
+      return "Week 2"
+    } else if (moment(MatchTime).isBetween('2016-09-21', '2016-09-28')) {
+      return "Week 3"
+    } else if (moment(MatchTime).isBetween('2016-09-28', '2016-10-05')) {
+      return "Week 4"
+    } else if (moment(MatchTime).isBetween('2016-10-05', '2016-10-12')) {
+      return "Week 5"
+    } else if (moment(MatchTime).isBetween('2016-10-12', '2016-10-19')) {
+      return "Week 6"
+    } else if (moment(MatchTime).isBetween('2016-10-19', '2016-10-26')) {
+      return "Week 7"
+    } else if (moment(MatchTime).isBetween('2016-10-26', '2016-11-02')) {
+      return "Week 8"
+    } else if (moment(MatchTime).isBetween('2016-11-02', '2016-11-09')) {
+      return "Week 9"
+    } else if (moment(MatchTime).isBetween('2016-11-09', '2016-11-16')) {
+      return "Week 10"
+    } else if (moment(MatchTime).isBetween('2016-11-16', '2016-11-23')) {
+      return "Week 11"
+    } else if (moment(MatchTime).isBetween('2016-11-23', '2016-11-30')) {
+      return "Week 12"
+    } else if (moment(MatchTime).isBetween('2016-11-30', '2016-12-07')) {
+      return "Week 13"
+    } else if (moment(MatchTime).isBetween('2016-12-07', '2016-12-14')) {
+      return "Week 14"
+    } else if (moment(MatchTime).isBetween('2016-12-14', '2016-12-21')) {
+      return "Week 15"
+    } else if (moment(MatchTime).isBetween('2016-12-21', '2016-12-28')) {
+      return "Week 16"
+    } else if (moment(MatchTime).isBetween('2016-12-28', '2017-01-04')) {
+      return "Week 17"
+    } else {
+      return "Postseason"
+    }
+  }
+
   vm.checkGametimes = function() {
     picksService.checkGametimes().then(function(result){
       return result
@@ -64,8 +106,16 @@ function PickController (oddsService, picksService, resultsService, authService)
       vm.weeksOfGames = dates;
       var dateArray = vm.weeksOfGames;
       var lastWeek = dateArray[dateArray.length - 1];
-      var currentWeek = oddsService.weekSetter(moment().format());
-      vm.gameWeekFilter = currentWeek;
+      var currentWeek = vm.weekSetter(moment().format());
+      if (currentWeek === "Preseason") {
+        vm.gameWeekFilter = "Week 1"
+      } else if (
+        currentWeek === "Postseason"
+      ) {
+        vm.gameWeekFilter = "Week 17"
+      } else {
+        vm.gameWeekFilter = currentWeek
+      }
     })
   };
 
@@ -124,6 +174,7 @@ function PickController (oddsService, picksService, resultsService, authService)
       vm.pick.favType = "Neither"
     }
     vm.pick.activeGame = game.EventID;
+    console.log('vm.pick.activeGame is', vm.pick.activeGame);
     vm.pick.activeSpread = game.PointSpreadAway;
     vm.pick.activeLine = game.PointSpreadAwayLine;
     vm.pick.activePick = (game.AwayAbbrev + ' ' +      vm.mlFormat(game.PointSpreadAway));
--- public/javascripts/controllers/results.controller.js
+++ public/javascripts/controllers/results.controller.js
@@ -91,6 +91,48 @@ function ResultController (oddsService, picksService, resultsService, usersServi
     }
   };
 
+  vm.weekSetter = function(MatchTime) {
+    if (moment(MatchTime).isBetween('2016-06-23', '2016-09-07')) {
+      return "Preseason"
+    } else if (moment(MatchTime).isBetween('2016-09-07', '2016-09-14')) {
+      return "Week 1"
+    } else if (moment(MatchTime).isBetween('2016-09-14', '2016-09-21')) {
+      return "Week 2"
+    } else if (moment(MatchTime).isBetween('2016-09-21', '2016-09-28')) {
+      return "Week 3"
+    } else if (moment(MatchTime).isBetween('2016-09-28', '2016-10-05')) {
+      return "Week 4"
+    } else if (moment(MatchTime).isBetween('2016-10-05', '2016-10-12')) {
+      return "Week 5"
+    } else if (moment(MatchTime).isBetween('2016-10-12', '2016-10-19')) {
+      return "Week 6"
+    } else if (moment(MatchTime).isBetween('2016-10-19', '2016-10-26')) {
+      return "Week 7"
+    } else if (moment(MatchTime).isBetween('2016-10-26', '2016-11-02')) {
+      return "Week 8"
+    } else if (moment(MatchTime).isBetween('2016-11-02', '2016-11-09')) {
+      return "Week 9"
+    } else if (moment(MatchTime).isBetween('2016-11-09', '2016-11-16')) {
+      return "Week 10"
+    } else if (moment(MatchTime).isBetween('2016-11-16', '2016-11-23')) {
+      return "Week 11"
+    } else if (moment(MatchTime).isBetween('2016-11-23', '2016-11-30')) {
+      return "Week 12"
+    } else if (moment(MatchTime).isBetween('2016-11-30', '2016-12-07')) {
+      return "Week 13"
+    } else if (moment(MatchTime).isBetween('2016-12-07', '2016-12-14')) {
+      return "Week 14"
+    } else if (moment(MatchTime).isBetween('2016-12-14', '2016-12-21')) {
+      return "Week 15"
+    } else if (moment(MatchTime).isBetween('2016-12-21', '2016-12-28')) {
+      return "Week 16"
+    } else if (moment(MatchTime).isBetween('2016-12-28', '2017-01-04')) {
+      return "Week 17"
+    } else {
+      return "Postseason"
+    }
+  }
+
   vm.sumWeek = function(user, weeknumb) {
     username = user.username;
     return picksService.sumWeek(username, weeknumb).then(function(result){
@@ -153,8 +195,18 @@ function ResultController (oddsService, picksService, resultsService, usersServi
       vm.weeksOfGames = dates;
       var dateArray = vm.weeksOfGames;
       var lastWeek = dateArray[dateArray.length - 1];
-      var currentWeek = oddsService.weekSetter(moment().format());
-      vm.gameWeekFilter = currentWeek;
+      var currentWeek = vm.weekSetter(moment().format());
+      if (currentWeek === "Preseason") {
+        vm.gameWeekFilter = "Week 1"
+      } else if (
+        currentWeek === "Postseason"
+      ) {
+        vm.gameWeekFilter = "Week 17"
+      } else {
+        vm.gameWeekFilter = currentWeek
+      }
+      // vm.gameWeekFilter = currentWeek;
+      // console.log('current week is', vm.gameWeekFilter);
     })
   };
 
