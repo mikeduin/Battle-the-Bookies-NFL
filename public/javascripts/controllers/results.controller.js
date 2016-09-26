f632e6ef2834f15462987a96643d53517f4f9acb
Mike Duin <michael.s.duin@gmail.com>
2016-09-26 15:39:57 -0700

    restructure result sort select using ng-repeat

--- public/javascripts/controllers/results.controller.js
+++ public/javascripts/controllers/results.controller.js
@@ -4,8 +4,6 @@ angular
 
 function ResultController (oddsService, picksService, resultsService, usersService, $scope, $timeout, $stateParams, $state) {
 
-  console.log($state);
-
   $scope.uiRouterState = $state;
   var vm = this;
   vm.gameWeekFilter;
@@ -34,13 +32,33 @@ function ResultController (oddsService, picksService, resultsService, usersServi
   // })
   $timeout(function(){
     vm.showSpinner = false
-  }, 6000)
+  }, 6000);
 
-  console.log('$stateParams are', $stateParams.weekNumb);
+  vm.weekValues = [
+    {"value": "-w1dollars", "text": "Week 1 $", "weekNumb": "01"},
+    {"value": "-w2dollars", "text": "Week 2 $", "weekNumb": "02"},
+    {"value": "-w3dollars", "text": "Week 3 $", "weekNumb": "03"},
+    {"value": "-w4dollars", "text": "Week 4 $", "weekNumb": "04"},
+    {"value": "-w5dollars", "text": "Week 5 $", "weekNumb": "05"},
+    {"value": "-w6dollars", "text": "Week 6 $", "weekNumb": "06"},
+    {"value": "-w7dollars", "text": "Week 7 $", "weekNumb": "07"},
+    {"value": "-w8dollars", "text": "Week 8 $", "weekNumb": "08"},
+    {"value": "-w9dollars", "text": "Week 9 $", "weekNumb": "09"},
+    {"value": "-w10dollars", "text": "Week 10 $", "weekNumb": "10"},
+    {"value": "-w11dollars", "text": "Week 11 $", "weekNumb": "11"},
+    {"value": "-w12dollars", "text": "Week 12 $", "weekNumb": "12"},
+    {"value": "-w13dollars", "text": "Week 13 $", "weekNumb": "13"},
+    {"value": "-w14dollars", "text": "Week 14 $", "weekNumb": "14"},
+    {"value": "-w15dollars", "text": "Week 15 $", "weekNumb": "15"},
+    {"value": "-w16dollars", "text": "Week 16 $", "weekNumb": "16"},
+    {"value": "-w17dollars", "text": "Week 17 $", "weekNumb": "17"},
+  ]
 
   vm.checkWeekNumb = function(){
     vm.weekNumb = $stateParams.weekNumb;
     vm.week = parseInt(vm.weekNumb);
+    console.log('weeknumb is', vm.weekNumb);
+    console.log('vm.userSort is ', vm.userSort)
   };
   vm.checkWeekNumb();
 
@@ -56,7 +74,8 @@ function ResultController (oddsService, picksService, resultsService, usersServi
 
   vm.getAllUsers = function(){
     usersService.getAllUsers().then(function(result){
-      vm.users = result
+      vm.users = result;
+      console.log(vm.users);
     })
   };
 
