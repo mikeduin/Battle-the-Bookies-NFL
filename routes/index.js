86b7aea41bc77ca7fc37801dd2324efe5c484886
Mike Duin <michael.s.duin@gmail.com>
2016-10-16 23:08:10 -0700

    rebuild pick array function

--- routes/index.js
+++ routes/index.js
@@ -356,103 +356,103 @@ router.get('/pullGame/:gameID', function(req, res, next){
 
 // The function below runs every 9 minutes and checks to see if a game has started and has not yet had its pick arrays built. In that case, it constructs the pick arrays and then marks ArraysBuilt as 'true' so as not to needlessly reproduce the update in the future.
 
-// setInterval(function(){
-//   var now = moment();
-//   Line.find({
-//     MatchTime: {
-//       $lt: now
-//     },
-//     ArraysBuilt: {
-//       $in: [false, null]
-//     }
-//   }, function(err, games){
-//     if(err) {console.log(err)}
-//
-//   }).then(function(games){
-//     games.forEach(function(game){
-//       var overPickArray = [];
-//       var underPickArray = [];
-//       var homeSpreadPickArray = [];
-//       var awaySpreadPickArray = [];
-//       var homeMLPickArray = [];
-//       var awayMLPickArray = [];
-//       var noPickArray = [];
-//       Pick.find({EventID: game.EventID}, function(err, picks){
-//         if(err) {console.log(err)}
-//
-//       }).then(function(picks){
-//         picks.forEach(function(pick){
-//           var relevantLine;
-//           if (pick.pickType === "Total Over" || pick.pickType === "Total Under") {
-//             relevantLine = pick.activeTotal
-//           } else if (pick.pickType === "Home Spread" || pick.pickType === "Away Spread") {
-//             relevantLine = pick.activeSpread
-//           } else {
-//             relevantLine = pick.activeLine
-//           };
-//           var pickObject = {
-//             id: pick.id,
-//             username: pick.username,
-//             submittedAt: pick.submittedAt,
-//             pickType: pick.pickType,
-//             geoType: pick.geoType,
-//             betType: pick.betType,
-//             favType: pick.favType,
-//             activePayout: pick.activePayout,
-//             activePick: pick.activePick,
-//             activeLine: pick.activeLine,
-//             capperGrade: pick.capperGrade,
-//             relevantLine: relevantLine
-//           };
-//           if (pick.pickType === "Total Over") {
-//             overPickArray.push(pickObject)
-//           } else if (pick.pickType === "Total Under") {
-//             underPickArray.push(pickObject)
-//           } else if (pick.pickType === "Away Spread") {
-//             awaySpreadPickArray.push(pickObject)
-//           } else if (pick.pickType === "Home Spread") {
-//             homeSpreadPickArray.push(pickObject)
-//           } else if (pick.pickType === "Away ML") {
-//             awayMLPickArray.push(pickObject)
-//           } else if (pick.pickType === "Home ML") {
-//             homeMLPickArray.push(pickObject)
-//           } else {
-//             noPickArray.push(pickObject)
-//           };
-//         });
-//
-//       }).then(function(){
-//         PickArray.findOneAndUpdate({EventID: game.EventID}, {
-//           $set: {
-//             EventID: game.EventID,
-//             OverPickArray: overPickArray,
-//             UnderPickArray: underPickArray,
-//             AwaySpreadPickArray: awaySpreadPickArray,
-//             HomeSpreadPickArray: homeSpreadPickArray,
-//             AwayMLPickArray: awayMLPickArray,
-//             HomeMLPickArray: homeMLPickArray,
-//             NoPickArray: noPickArray
-//           }
-//         }, {upsert: true}, function(err, result){
-//           if(err) {console.log(err)}
-//
-//           console.log("arrays have been built for", result.EventID)
-//
-//         })
-//       }).then(function(){
-//         Line.findOneAndUpdate({EventID: game.EventID}, {$set:
-//           {
-//             ArraysBuilt: true
-//           }
-//         }, function(err, updatedLine){
-//           if (err) {console.log(err)}
-//
-//           console.log('arrays built set to true for', updatedLine.EventID)
-//         })
-//       })
-//     })
-//   })
-// }, 540000)
+setInterval(function(){
+  var now = moment();
+  Line.find({
+    MatchTime: {
+      $lt: now
+    },
+    ArraysBuilt: {
+      $in: [false, null]
+    }
+  }, function(err, games){
+    if(err) {console.log(err)}
+
+  }).then(function(games){
+    games.forEach(function(game){
+      var overPickArray = [];
+      var underPickArray = [];
+      var homeSpreadPickArray = [];
+      var awaySpreadPickArray = [];
+      var homeMLPickArray = [];
+      var awayMLPickArray = [];
+      var noPickArray = [];
+      Pick.find({EventID: game.EventID}, function(err, picks){
+        if(err) {console.log(err)}
+
+      }).then(function(picks){
+        picks.forEach(function(pick){
+          var relevantLine;
+          if (pick.pickType === "Total Over" || pick.pickType === "Total Under") {
+            relevantLine = pick.activeTotal
+          } else if (pick.pickType === "Home Spread" || pick.pickType === "Away Spread") {
+            relevantLine = pick.activeSpread
+          } else {
+            relevantLine = pick.activeLine
+          };
+          var pickObject = {
+            id: pick.id,
+            username: pick.username,
+            submittedAt: pick.submittedAt,
+            pickType: pick.pickType,
+            geoType: pick.geoType,
+            betType: pick.betType,
+            favType: pick.favType,
+            activePayout: pick.activePayout,
+            activePick: pick.activePick,
+            activeLine: pick.activeLine,
+            capperGrade: pick.capperGrade,
+            relevantLine: relevantLine
+          };
+          if (pick.pickType === "Total Over") {
+            overPickArray.push(pickObject)
+          } else if (pick.pickType === "Total Under") {
+            underPickArray.push(pickObject)
+          } else if (pick.pickType === "Away Spread") {
+            awaySpreadPickArray.push(pickObject)
+          } else if (pick.pickType === "Home Spread") {
+            homeSpreadPickArray.push(pickObject)
+          } else if (pick.pickType === "Away Moneyline") {
+            awayMLPickArray.push(pickObject)
+          } else if (pick.pickType === "Home Moneyline") {
+            homeMLPickArray.push(pickObject)
+          } else {
+            noPickArray.push(pickObject)
+          };
+        });
+
+      }).then(function(){
+        PickArray.findOneAndUpdate({EventID: game.EventID}, {
+          $set: {
+            EventID: game.EventID,
+            OverPickArray: overPickArray,
+            UnderPickArray: underPickArray,
+            AwaySpreadPickArray: awaySpreadPickArray,
+            HomeSpreadPickArray: homeSpreadPickArray,
+            AwayMLPickArray: awayMLPickArray,
+            HomeMLPickArray: homeMLPickArray,
+            NoPickArray: noPickArray
+          }
+        }, {upsert: true}, function(err){
+          if(err) {console.log(err)}
+
+          console.log("arrays have been built for", game.EventID)
+
+        })
+      }).then(function(){
+        Line.findOneAndUpdate({EventID: game.EventID}, {$set:
+          {
+            ArraysBuilt: true
+          }
+        }, function(err, updatedLine){
+          if (err) {console.log(err)}
+
+          console.log('arrays built set to true for', updatedLine.EventID)
+        })
+      })
+    })
+  })
+}, 10000)
 
 // This function below runs every 14 minutes and checks to see whether a game's pick ranges have been added to the original line data.
 
@@ -599,7 +599,7 @@ router.get('/pullGame/:gameID', function(req, res, next){
 //       })
 //     })
 //   })
-// }, 10000)
+// }, 54000000000)
 
 // ALL THE FUNCTION BELOW DOES IS UPDATE THE LINE DATA WITH THE RANGES. DO YOU EVEN NEED TO DO THAT ANYMORE NOW THAT YOU ARE TRACKING LINE MOVEMENT?
 
