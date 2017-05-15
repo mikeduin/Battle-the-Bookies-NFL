97f3b0f052c6de626e23a856bd9224a93549aa1e
Mike Duin <michael.s.duin@gmail.com>
2017-05-14 23:11:01 -0700

    add psql + users migration

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/btb-nfl'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  }
+
+};
--- toggle.js
+++ toggle.js
@@ -1,11 +1,6 @@
-var person = function (age, weight) {
-  this.age = age;
-  this.weight = weight;
-  this.getInfo = function() {
-    console.log("I am " + this.age + " years ols and I weight " + this.weight + " pounds and I get paid " + this.salary);
+function sum (n) {
+  if (n<=0) {
+    return 0
   };
-}
-
-employee.getInfo = function() {
-  console.log("I am " + this.age + " years ols and I weight " + this.weight + " pounds and I get paid " + this.salary);
+  return n + sum(n-1)
 };
