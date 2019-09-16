3222e26017ffff63a1c13c820125d9256884f5ba
Mike Duin <michael.s.duin@gmail.com>
2019-09-15 22:13:59 -0700

    update server-side week-setting modules

--- modules/weekNumbSetter.js
+++ modules/weekNumbSetter.js
@@ -4,83 +4,42 @@ module.exports = {
   weekNumbSetter: function (matchTime) {
     if (moment(matchTime).isBefore('2019-08-30')) {
       return "PRE"
-    } else if (moment(matchTime).isBetween('2019-09-01', '2019-09-10')) {
+    } else if (moment(matchTime).isBetween('2019-09-01', '2019-09-11')) {
       return "01"
-    } else if (moment(matchTime).isBetween('2019-09-10', '2019-09-17')) {
+    } else if (moment(matchTime).isBetween('2019-09-11', '2019-09-18')) {
       return "02"
-    } else if (moment(matchTime).isBetween('2019-09-17', '2019-09-24')) {
+    } else if (moment(matchTime).isBetween('2019-09-18', '2019-09-25')) {
       return "03"
-    } else if (moment(matchTime).isBetween('2019-09-24', '2019-10-01')) {
+    } else if (moment(matchTime).isBetween('2019-09-25', '2019-10-02')) {
       return "04"
-    } else if (moment(matchTime).isBetween('2019-10-01', '2019-10-08')) {
+    } else if (moment(matchTime).isBetween('2019-10-02', '2019-10-09')) {
       return "05"
-    } else if (moment(matchTime).isBetween('2019-10-08', '2019-10-15')) {
+    } else if (moment(matchTime).isBetween('2019-10-09', '2019-10-16')) {
       return "06"
-    } else if (moment(matchTime).isBetween('2019-10-15', '2019-10-22')) {
+    } else if (moment(matchTime).isBetween('2019-10-16', '2019-10-23')) {
       return "07"
-    } else if (moment(matchTime).isBetween('2019-10-22', '2019-10-29')) {
+    } else if (moment(matchTime).isBetween('2019-10-23', '2019-10-30')) {
       return "08"
-    } else if (moment(matchTime).isBetween('2019-10-29', '2019-11-05')) {
+    } else if (moment(matchTime).isBetween('2019-10-30', '2019-11-06')) {
       return "09"
-    } else if (moment(matchTime).isBetween('2019-11-05', '2019-11-12')) {
+    } else if (moment(matchTime).isBetween('2019-11-06', '2019-11-13')) {
       return "10"
-    } else if (moment(matchTime).isBetween('2019-11-12', '2019-11-19')) {
+    } else if (moment(matchTime).isBetween('2019-11-13', '2019-11-20')) {
       return "11"
-    } else if (moment(matchTime).isBetween('2019-11-19', '2019-11-26')) {
+    } else if (moment(matchTime).isBetween('2019-11-20', '2019-11-27')) {
       return "12"
-    } else if (moment(matchTime).isBetween('2019-11-26', '2019-12-03')) {
+    } else if (moment(matchTime).isBetween('2019-11-27', '2019-12-04')) {
       return "13"
-    } else if (moment(matchTime).isBetween('2019-12-03', '2019-12-10')) {
+    } else if (moment(matchTime).isBetween('2019-12-04', '2019-12-11')) {
       return "14"
-    } else if (moment(matchTime).isBetween('2019-12-10', '2019-12-17')) {
+    } else if (moment(matchTime).isBetween('2019-12-11', '2019-12-18')) {
       return "15"
-    } else if (moment(matchTime).isBetween('2019-12-17', '2019-12-24')) {
+    } else if (moment(matchTime).isBetween('2019-12-18', '2019-12-25')) {
       return "16"
-    } else if (moment(matchTime).isBetween('2019-12-24', '2020-01-02')) {
+    } else if (moment(matchTime).isBetween('2019-12-25', '2020-01-03')) {
       return "17"
     } else {
       return "POST"
     };
   }
-  // {
-  //   if (moment(MatchTime).isBetween('2018-06-23', '2018-09-01')) {
-  //     return "PRE"
-  //   } else if (moment(MatchTime).isBetween('2018-09-02', '2018-09-12')) {
-  //     return "01"
-  //   } else if (moment(MatchTime).isBetween('2018-09-12', '2018-09-19')) {
-  //     return "02"
-  //   } else if (moment(MatchTime).isBetween('2018-09-19', '2018-09-26')) {
-  //     return "03"
-  //   } else if (moment(MatchTime).isBetween('2018-09-26', '2018-10-03')) {
-  //     return "04"
-  //   } else if (moment(MatchTime).isBetween('2018-10-03', '2018-10-10')) {
-  //     return "05"
-  //   } else if (moment(MatchTime).isBetween('2018-10-10', '2018-10-17')) {
-  //     return "06"
-  //   } else if (moment(MatchTime).isBetween('2018-10-17', '2018-10-24')) {
-  //     return "07"
-  //   } else if (moment(MatchTime).isBetween('2018-10-24', '2018-10-31')) {
-  //     return "08"
-  //   } else if (moment(MatchTime).isBetween('2018-10-31', '2018-11-07')) {
-  //     return "09"
-  //   } else if (moment(MatchTime).isBetween('2018-11-07', '2018-11-14')) {
-  //     return "10"
-  //   } else if (moment(MatchTime).isBetween('2018-11-14', '2018-11-21')) {
-  //     return "11"
-  //   } else if (moment(MatchTime).isBetween('2018-11-21', '2018-11-28')) {
-  //     return "12"
-  //   } else if (moment(MatchTime).isBetween('2018-11-28', '2018-12-05')) {
-  //     return "13"
-  //   } else if (moment(MatchTime).isBetween('2018-12-05', '2018-12-12')) {
-  //     return "14"
-  //   } else if (moment(MatchTime).isBetween('2018-12-12', '2018-12-19')) {
-  //     return "15"
-  //   } else if (moment(MatchTime).isBetween('2018-12-19', '2018-12-26')) {
-  //     return "16"
-  //   } else if (moment(MatchTime).isBetween('2018-12-26', '2019-01-02')) {
-  //     return "17"
-  //   } else {
-  //     return "POST"
-  //   }
-  // }
 }
--- modules/weekSetter.js
+++ modules/weekSetter.js
@@ -4,39 +4,39 @@ module.exports = {
   weekSetter: function (matchTime) {
     if (moment(matchTime).isBefore('2019-08-30')) {
       return "Preseason"
-    } else if (moment(matchTime).isBetween('2019-09-01', '2019-09-10')) {
+    } else if (moment(matchTime).isBetween('2019-09-01', '2019-09-11')) {
       return "Week 1"
-    } else if (moment(matchTime).isBetween('2019-09-10', '2019-09-17')) {
+    } else if (moment(matchTime).isBetween('2019-09-11', '2019-09-18')) {
       return "Week 2"
-    } else if (moment(matchTime).isBetween('2019-09-17', '2019-09-24')) {
+    } else if (moment(matchTime).isBetween('2019-09-18', '2019-09-25')) {
       return "Week 3"
-    } else if (moment(matchTime).isBetween('2019-09-24', '2019-10-01')) {
+    } else if (moment(matchTime).isBetween('2019-09-25', '2019-10-02')) {
       return "Week 4"
-    } else if (moment(matchTime).isBetween('2019-10-01', '2019-10-08')) {
+    } else if (moment(matchTime).isBetween('2019-10-02', '2019-10-09')) {
       return "Week 5"
-    } else if (moment(matchTime).isBetween('2019-10-08', '2019-10-15')) {
+    } else if (moment(matchTime).isBetween('2019-10-09', '2019-10-16')) {
       return "Week 6"
-    } else if (moment(matchTime).isBetween('2019-10-15', '2019-10-22')) {
+    } else if (moment(matchTime).isBetween('2019-10-16', '2019-10-23')) {
       return "Week 7"
-    } else if (moment(matchTime).isBetween('2019-10-22', '2019-10-29')) {
+    } else if (moment(matchTime).isBetween('2019-10-23', '2019-10-30')) {
       return "Week 8"
-    } else if (moment(matchTime).isBetween('2019-10-29', '2019-11-05')) {
+    } else if (moment(matchTime).isBetween('2019-10-30', '2019-11-06')) {
       return "Week 9"
-    } else if (moment(matchTime).isBetween('2019-11-05', '2019-11-12')) {
+    } else if (moment(matchTime).isBetween('2019-11-06', '2019-11-13')) {
       return "Week 10"
-    } else if (moment(matchTime).isBetween('2019-11-12', '2019-11-19')) {
+    } else if (moment(matchTime).isBetween('2019-11-13', '2019-11-20')) {
       return "Week 11"
-    } else if (moment(matchTime).isBetween('2019-11-19', '2019-11-26')) {
+    } else if (moment(matchTime).isBetween('2019-11-20', '2019-11-27')) {
       return "Week 12"
-    } else if (moment(matchTime).isBetween('2019-11-26', '2019-12-03')) {
+    } else if (moment(matchTime).isBetween('2019-11-27', '2019-12-04')) {
       return "Week 13"
-    } else if (moment(matchTime).isBetween('2019-12-03', '2019-12-10')) {
+    } else if (moment(matchTime).isBetween('2019-12-04', '2019-12-11')) {
       return "Week 14"
-    } else if (moment(matchTime).isBetween('2019-12-10', '2019-12-17')) {
+    } else if (moment(matchTime).isBetween('2019-12-11', '2019-12-18')) {
       return "Week 15"
-    } else if (moment(matchTime).isBetween('2019-12-17', '2019-12-24')) {
+    } else if (moment(matchTime).isBetween('2019-12-18', '2019-12-25')) {
       return "Week 16"
-    } else if (moment(matchTime).isBetween('2019-12-24', '2020-01-02')) {
+    } else if (moment(matchTime).isBetween('2019-12-25', '2020-01-03')) {
       return "Week 17"
     } else {
       return "Postseason"
