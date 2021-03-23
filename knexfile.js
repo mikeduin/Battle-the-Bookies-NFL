f49ee3382eb2f2c7572f63bb71a18b202a96a7fc
Michael Duin <michael.s.duin@gmail.com>
2021-03-23 00:17:36 -0700

    update pg, knex

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,19 @@
 // Update with your config settings.
 require('dotenv').config();
+const pg = require('pg')
+pg.defaults.ssl = {
+   rejectUnauthorized: false,
+}
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     // connection: process.env.TEST_DB_URL
+    ssl: {
+      rejectUnauthorized: false,  
+    },
+    ssl: 'no-verify' // try this if above does not work for you
   },
 
   production: {
